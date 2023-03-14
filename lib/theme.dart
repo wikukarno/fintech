2b6eee1a8491a43561e213c696f3d742b6848c63
wikukarno <prasetyagama2@gmail.com>
2023-03-14 08:12:24 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24;
+
+// NOTE: COLORS
+const greenColor = Color(0xff104E5B);
+const whiteColor = Color(0xffFFFFFF);
+const blackColor = Color(0xff000000);
+const greyColor = Color(0xff616F80);
+const redColor = Color(0xffFF7878);
+const yellowColor = Color(0xffFDC27A);
+
+// NOTE: TEXT STYLES
+
+TextStyle whiteTextStyle = GoogleFonts.saira(
+  color: whiteColor,
+);
+
+TextStyle blackTextStyle = GoogleFonts.saira(
+  color: blackColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.saira(
+  color: greyColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.saira(
+  color: redColor,
+);
+
+// NOTE: FONT WEIGHTS
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
